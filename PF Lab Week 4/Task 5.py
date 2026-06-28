89ef9d162c00b7b3c0e5a1f97081ebde1e2ad3f3
Abdul Moqeet <muqeetawan167@gmail.com>
2026-06-28 11:25:10 +0500

    Delete PF Lab Week 4 directory

--- PF Lab Week 4/Task 5.py
+++
@@ -1,7 +0,0 @@
-principal = float(input("Enter principal amount: "))
-rate = float(input("Enter rate of interest: "))
-time = float(input("Enter time in years: "))
-
-interest = (principal * rate * time) / 100
-
-print("Simple Interest =", interest)
--- PF Lab Week 4/Task 6.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a three-digit number: "))
-
-hundreds = number // 100
-tens = (number // 10) % 10
-ones = number % 10
-
-reverse = (ones * 100) + (tens * 10) + hundreds
-
-print("Reversed Number =", reverse)
