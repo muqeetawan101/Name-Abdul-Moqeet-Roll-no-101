c4814f14c78f69bd97f4f6667a1ae00efdc2e556
Abdul Moqeet <muqeetawan167@gmail.com>
2026-06-28 11:24:43 +0500

    Delete PF Lab Week 13 directory

--- PF Lab Week 13/Task 1.py
+++
@@ -1,8 +0,0 @@
-try:
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    result = num1 / num2
-    print("Result:", result)
-
-except ZeroDivisionError:
-    print("Error: You cannot divide by zero.")
