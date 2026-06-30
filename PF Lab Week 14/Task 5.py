e641644cf99971528991022ea34507bf76144897
Abdul Moqeet <muqeetawan167@gmail.com>
2026-06-30 19:14:57 +0500

    Delete PF Lab Week 14 directory

--- PF Lab Week 14/Task 5.py
+++
@@ -1,6 +0,0 @@
-numbers = [10, 20, 20, 30, 40, 40, 50, 60, 60]
-
-unique_numbers = set(numbers)
-
-print("Original List:", numbers)
-print("Unique Elements:", unique_numbers)
--- PF Lab Week 14/Task 7.py
+++
@@ -1,7 +0,0 @@
-classA = {"Ali", "Ahmed", "Sara", "Ayesha"}
-classB = {"Sara", "Ali", "Usman", "Zain"}
-
-common_students = classA & classB
-
-print("Students in Both Classes:")
-print(common_students)
