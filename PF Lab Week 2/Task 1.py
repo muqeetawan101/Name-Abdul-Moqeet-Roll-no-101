a8466483102e0d15e2e436c237374b5ebf8a537f
Abdul Moqeet <muqeetawan167@gmail.com>
2026-06-30 19:15:42 +0500

    Delete PF Lab Week 2 directory

--- PF Lab Week 2/Task 1.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-
-total = a + b + c
-
-print("Total =", total)
--- PF Lab Week 2/Task 5.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Enter a number: "))
-
-remainder = number % 2
-
-print("Remainder =", remainder)
